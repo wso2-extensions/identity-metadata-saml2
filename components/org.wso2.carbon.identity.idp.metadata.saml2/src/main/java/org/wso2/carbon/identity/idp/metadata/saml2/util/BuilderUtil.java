bf58b182ceb9f1cb60f293a771a17ef7fc26f8db
deshankoswatte <dehami.deshan@gmail.com>
2019-10-14 17:42:05 +0530

    Revamped bootstrap methods to use SAMLInitializer at carbon.identity.core.util

--- components/org.wso2.carbon.identity.idp.metadata.saml2/src/main/java/org/wso2/carbon/identity/idp/metadata/saml2/util/BuilderUtil.java
+++ components/org.wso2.carbon.identity.idp.metadata.saml2/src/main/java/org/wso2/carbon/identity/idp/metadata/saml2/util/BuilderUtil.java
@@ -20,11 +20,11 @@ package org.wso2.carbon.identity.idp.metadata.saml2.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.opensaml.core.config.InitializationService;
 import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
 import org.opensaml.core.config.InitializationException;
 import org.opensaml.core.xml.XMLObjectBuilderFactory;
 
+import org.wso2.carbon.identity.core.util.SAMLInitializer;
 import org.wso2.carbon.idp.mgt.MetadataException;
 import javax.xml.namespace.QName;
 
@@ -39,47 +39,12 @@ public class BuilderUtil {
 
     public static void doBootstrap() {
         if (!isBootStrapped) {
-
-            Thread thread = Thread.currentThread();
-            ClassLoader loader = thread.getContextClassLoader();
-            thread.setContextClassLoader(InitializationService.class.getClassLoader());
-
             try {
-                InitializationService.initialize();
-
-                org.opensaml.saml.config.SAMLConfigurationInitializer initializer_1 = new org.opensaml.saml.config.SAMLConfigurationInitializer();
-                initializer_1.init();
-
-                org.opensaml.saml.config.XMLObjectProviderInitializer initializer_2 = new org.opensaml.saml.config.XMLObjectProviderInitializer();
-                initializer_2.init();
-
-                org.opensaml.core.xml.config.XMLObjectProviderInitializer initializer_3 = new org.opensaml.core.xml.config.XMLObjectProviderInitializer();
-                initializer_3.init();
-
-                org.opensaml.core.xml.config.GlobalParserPoolInitializer initializer_4 = new org.opensaml.core.xml.config.GlobalParserPoolInitializer();
-                initializer_4.init();
-
-                org.opensaml.xmlsec.config.JavaCryptoValidationInitializer initializer_5 = new org.opensaml.xmlsec.config.JavaCryptoValidationInitializer();
-                initializer_5.init();
-
-                org.opensaml.xmlsec.config.XMLObjectProviderInitializer initializer_6 = new org.opensaml.xmlsec.config.XMLObjectProviderInitializer();
-                initializer_6.init();
-
-                org.opensaml.xmlsec.config.ApacheXMLSecurityInitializer initializer_7 = new org.opensaml.xmlsec.config.ApacheXMLSecurityInitializer();
-                initializer_7.init();
-
-                org.opensaml.xmlsec.config.GlobalSecurityConfigurationInitializer initializer_8 = new org.opensaml.xmlsec.config.GlobalSecurityConfigurationInitializer();
-                initializer_8.init();
-
-                org.opensaml.xmlsec.config.GlobalAlgorithmRegistryInitializer initializer_9 = new org.opensaml.xmlsec.config.GlobalAlgorithmRegistryInitializer();
-                initializer_9.init();
+                SAMLInitializer.doBootstrap();
                 isBootStrapped = true;
             } catch (InitializationException e) {
                 log.error("Error in bootstrapping the OpenSAML3 library", e);
-            } finally {
-                thread.setContextClassLoader(loader);
             }
-
         }
     }
 
